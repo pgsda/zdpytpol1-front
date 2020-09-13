3e7e1c1c04651a520a68411ddc0d4369d34031ba
Your Name <you@example.com>
2020-09-13 12:23:38 +0200

    js - czesc dalsza

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+var buttons = document.querySelectorAll("header input");
+
+buttons[0].addEventListener("click", changeHeader);
+buttons[1].addEventListener("mouseover", changeAdd);
+
 function changeHeader() {
-    document.getElementById("naglowek").innerHTML = "nowy tekst";
+    var naglowek = document.getElementById("naglowek");
+    naglowek.innerHTML = "nowy tekst <i>to jest tekst pochylony</i> a to nie";
+    naglowek.style.color = "red";
+    naglowek.style.fontSize = "1em";
+}
+
+function changeAdd() {
+    var add = document.querySelector("header h2");
+    add.innerHTML = "To jest nowe haslo reklamowe";
+    add.style.color = "blue";
 }
