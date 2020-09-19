4513ec7f840aeeebbb8df9cbcd0860e5205c677b
Your Name <you@example.com>
2020-09-19 13:18:35 +0200

    rozwiniecie komentarzy

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+comments_list = [
+    {"name": "Jan", "surname": "Nowak", "content": "Jestem bardzo zadowolony!", "grade": 5}
+]
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -9,3 +13,7 @@ def index():
 @app.route('/produkty')
 def products():
     return render_template('produkty.html')
+
+@app.route('/komentarze')
+def comments():
+    return render_template('komentarze.html', cl=comments_list)
